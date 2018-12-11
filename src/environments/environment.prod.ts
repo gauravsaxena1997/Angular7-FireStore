171a3a33673a3db007d84eba1065d9a113b97a13
Gaurav Saxena <32961153+gurvsxen487@users.noreply.github.com>
2018-12-11 16:40:06 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
     firebaseConfig: {
-    apiKey: "AIzaSyDLnA4I9Km96uzxcnFrFLuA6C1CgPsyBsg",
-    authDomain: "angular7-firestore.firebaseapp.com",
-    databaseURL: "https://angular7-firestore.firebaseio.com",
-    projectId: "angular7-firestore",
-    storageBucket: "angular7-firestore.appspot.com",
-    messagingSenderId: "156797690863"  	
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""  	
   }
 };
